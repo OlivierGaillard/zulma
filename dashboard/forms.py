c3658a2d320293dc87755ed910f139abd8792a0f
oliviergaillard <info@metacoherence.ch>
2018-04-12 14:06:10 +0200

    bootstrap added to dates form for dashboards.

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,9 +1,26 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.bootstrap import TabHolder, Tab, FormActions
+from crispy_forms.layout import Submit, Layout, Fieldset, Field
 from datetime import date
 
 class DateForm(forms.Form):
     start_date = forms.DateField(required=False, widget=forms.TextInput(attrs={ 'type' : 'date' }))
     end_date =   forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
+
+    def __init__(self, *args, **kwargs):
+        super(DateForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = "GET"
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-4'
+        self.helper.layout.append(
+            FormActions(
+                Submit('save', 'Submit'),
+            )
+        )
+
 
     def clean(self):
         cleaned_data = super().clean()
