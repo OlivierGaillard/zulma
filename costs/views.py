6a2e88d05c5abeffe4c8e5670973c478931e299f
oliviergaillard <info@metacoherence.ch>
2018-04-18 16:22:12 +0200

    New filter for Costs and link from the branch its costs.

--- costs/views.py
+++ costs/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView, TemplateView
 from .models import Category, Enterprise, Costs
 from .forms import CategoryCreateForm, CategoryUpdateForm, EnterpriseCreateForm, EnterpriseUpdateForm, CostsCreateForm, CostsUpdateForm
-
+from inventory.models import Branch
 
 class CategoryDetailView(DetailView):
     model = Category
@@ -89,10 +89,41 @@ class CostsListView(ListView):
     template_name = 'costs/costs.html'
     context_object_name = 'costs'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, q=None):
         ctx = super(CostsListView, self).get_context_data()
-        ctx['total'] = Costs.objects.total_costs()
+        q = self.request.GET.get('q')
+        if q:
+            ctx['option'] = q
+            if q.upper() == 'MAIN':
+                # costs = Costs.objects.filter(branch=None)
+                # total = sum(c.amount for c in costs)
+                total = Costs.objects.total_costs(branch='MAIN')
+                ctx['total'] = total
+            elif q.upper() == 'ALL':
+                ctx['total'] = Costs.objects.total_costs()
+            else:
+                branch = Branch.objects.get(name=q)
+                ctx['total'] = Costs.objects.total_costs(branch=branch)
+        else:
+            ctx['total'] = Costs.objects.total_costs()
+        li = [b.name for b in Branch.objects.all()] + ['All', 'Main']
+        li.sort()
+        ctx['branchs'] = li
         return ctx
+
+    def get_queryset(self):
+        queryset = super(CostsListView, self).get_queryset()
+        #ctx = super(CostsListView, self).get_context_data()
+        q = self.request.GET.get('q')
+        if q:
+            if q.upper() == 'MAIN':
+                queryset = queryset.filter(branch=None)
+            elif q.upper() == 'ALL':
+                pass
+            else:
+                queryset = queryset.filter(branch__name__icontains=q)
+        return queryset
+
 
 class CostsPerBranch(TemplateView):
     template_name = 'costs/costs.html'
