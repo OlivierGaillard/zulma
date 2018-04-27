7f4a617cc894d3fe12b6880dc23dec9529acd726
oliviergaillard <info@metacoherence.ch>
2018-04-27 08:55:17 +0200

    Dashboard translation

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,10 +3,11 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import TabHolder, Tab, FormActions
 from crispy_forms.layout import Submit, Layout, Fieldset, Field
 from datetime import date
+from django.utils.translation import ugettext_lazy as _
 
 class DateForm(forms.Form):
-    start_date = forms.DateField(required=False, widget=forms.TextInput(attrs={ 'type' : 'date' }))
-    end_date =   forms.DateField(required=False, widget=forms.TextInput(attrs={'type': 'date'}))
+    start_date = forms.DateField(label=_('Start Date'), required=False, widget=forms.TextInput(attrs={ 'type' : 'date' }))
+    end_date =   forms.DateField(label=_('End Date'),required=False, widget=forms.TextInput(attrs={'type': 'date'}))
 
     def __init__(self, *args, **kwargs):
         super(DateForm, self).__init__(*args, **kwargs)
@@ -28,8 +29,8 @@ class DateForm(forms.Form):
         d2 = cleaned_data.get('end_date')
         if d1 and d2:
             if d1 > d2:
-                raise forms.ValidationError("start and end dates order mismatch")
+                raise forms.ValidationError(_("start and end dates order mismatch"))
         if not d1 and not d2:
-            raise forms.ValidationError('At least one date is required')
+            raise forms.ValidationError(_('At least one date is required'))
         return cleaned_data
 
